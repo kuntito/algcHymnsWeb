719ae3ad8a20850c94f8e1f03bd09ed19311eca0
kuntito <owolabitito@gmail.com>
2026-08-02 11:00:08 +0100

    add comp, content hymn list header

---
+++ src/components/screens/hymn_list/components/ContentHymnListHeader.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import FrameAppHeader from "../../../AppHeader"
+import AppSearchBar from "../../../AppSearchBar"
+import { Box, HStack } from "@chakra-ui/react";
+import OutlineButton from "../../../OutlineButton";
+import { AddIcon } from "../../../appIcons/AddIcon";
+
+const ContentHymnListHeader = () => {
+    // TODO should probably be in zustand slice
+    const [query, setQuery] = useState("");
+    const onClear = () => {
+        setQuery("");
+    }
+
+    const buttonText = "add hymn";
+    
+    return (
+        <FrameAppHeader>
+            <HStack
+                w={"100%"}
+                h={"100%"}
+                border={"1px solid red"}
+                justifyContent={"center"}
+                gap={"16px"}
+            >
+                {/* placeholder to balance the right icon */}
+                <Box
+                    visibility={"hidden"}
+                >
+                    <OutlineButton 
+                        icon={<AddIcon />}
+                        text={buttonText} 
+                        onClick={() => {}} 
+                    />
+                </Box>
+                <AppSearchBar 
+                    query={query}
+                    onQueryChange={setQuery}
+                    onClear={onClear}
+                />
+                <OutlineButton 
+                    icon={<AddIcon />}
+                    text={buttonText} 
+                    onClick={() => { }} 
+                />
+            </HStack>
+        </FrameAppHeader>
+    )
+}
+
+export default ContentHymnListHeader
